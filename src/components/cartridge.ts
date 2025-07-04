3b3d8db49f148b22e224aad7ad142fd963cf16f0
eric-j-owen <ejowen+github@proton.me>
2025-07-03 22:17:42 -0400

    adds carts and styles, updates styles

--- src/components/cartridge.ts
+++ src/components/cartridge.ts
@@ -4,31 +4,41 @@ class Cartridge extends HTMLElement {
   }
 
   connectedCallback(): void {
-    // link stylesheet
-    const linkElem = document.createElement("link");
-    linkElem.setAttribute("rel", "stylesheet");
-    linkElem.setAttribute("href", "styles/cartridge.css");
-
-    //shadow root
-    const shadow = this.attachShadow({ mode: "open" });
-
-    //container
+    //cart container
     const container = document.createElement("div");
     container.setAttribute("id", "cartridge");
+    container.setAttribute("class", "obj-3d");
 
+    //shape faces
     const faceClasses = ["front", "back", "left", "right", "top", "bottom"];
+
+    //construct cart shape
+    let frontFace: HTMLDivElement;
     faceClasses.forEach((faceClass) => {
       const face = document.createElement("div");
       face.classList.add("face", faceClass);
       container.appendChild(face);
+
+      if (faceClass === "front") {
+        frontFace = face;
+      }
     });
 
-    const coverArt = document.createElement("img");
+    // construct label
 
-    shadow.appendChild(linkElem);
+    const labelContainer = document.createElement("div");
+    labelContainer.classList.add("label-container");
 
-    container.appendChild(coverArt);
-    shadow.appendChild(container);
+    const imgUrl =
+      this.getAttribute("img") ??
+      "https://as2.ftcdn.net/jpg/02/51/95/53/1000_F_251955356_FAQH0U1y1TZw3ZcdPGybwUkH90a3VAhb.jpg";
+    const img = document.createElement("img");
+    img.src = imgUrl;
+    labelContainer.append(img);
+
+    frontFace!.appendChild(labelContainer);
+
+    this.appendChild(container);
   }
 }
 
